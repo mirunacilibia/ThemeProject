1158544691bb26a1385a20f63cb00fbd99062f42
iasminasilaschi <iasminasilaschi@yahoo.com>
2023-01-19 02:52:45 +0200

    final flickery ideal ratio

--- Assets/Scripts/FlickerController.cs
+++ Assets/Scripts/FlickerController.cs
@@ -18,10 +18,16 @@ public class FlickerController : MonoBehaviour
     {
         isFlickering = true;
         this.gameObject.GetComponent<Light>().enabled = false;
-        timeDelay = Random.Range(0.02f, 0.5f);
+        timeDelay = Random.Range(1.0f, 2.0f);
         yield return new WaitForSeconds(timeDelay);
         this.gameObject.GetComponent<Light>().enabled = true;
-        timeDelay = Random.Range(0.05f, 0.10f);
+        timeDelay = Random.Range(0.1f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = false;
+        timeDelay = Random.Range(0.1f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        timeDelay = Random.Range(0.1f, 0.2f);
         yield return new WaitForSeconds(timeDelay);
         isFlickering=false;
     }
